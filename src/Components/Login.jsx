eb647e84919a36120b2c6f6202fa717ab47f0191
apenas-will <wildis.filho@gmail.com>
2024-03-23 16:33:57 -0300

    Adding more stuff

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import imageSrc from "./assets/logo_b.png"
 
 const Login = (props) => {
     return (
-        <div className="login-container">
-            <h1 className="welcome-message">Welcome to decentralized voting application</h1>
-            <button className="login-button" onClick = {props.connectWallet}>Login Metamask</button>
+        <div className="general-background">
+            <div className="login-container" >
+                <img src={imageSrc} alt="Text" className="login-logo"/>
+                <h1 className="welcome-message">Welcome to your safe voting space</h1>
+                <button className="login-button" onClick = {props.connectWallet}>Login Metamask</button>
+            </div>
         </div>
     )
 }
