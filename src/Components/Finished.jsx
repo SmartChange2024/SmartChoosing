ef7dd2c0662054cd2e2397e21ad3faab0d595f6a
apenas-will <wildis.filho@gmail.com>
2024-03-24 01:27:52 -0300

    Página de finalização

--- src/Components/Finished.jsx
+++ src/Components/Finished.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import imageSrc from "./assets/logo_t_b.png"
 
 const Finished = (props) => {
     return (
-        <div className="login-container">
-            <h1 className="welcome-message">Voting is Finished</h1>
+        <div className="general-background">
+            <img src={imageSrc} alt="Text" className="login-logo"/>
+            <div className="login-container">
+                <h1 className="welcome-message">Voting is Finished</h1>
+                <p className="connected-account">See the results:</p>
+
+                <table id="resultsTable" className="candidates-table">
+                    <thead>
+                    <tr>
+                        <th>Index</th>
+                        <th>Candidate name</th>
+                        <th>Candidate votes</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {props.candidates.map((candidate, index) => (
+                        <tr key={index}>
+                        <td>{candidate.index}</td>
+                        <td>{candidate.name}</td>
+                        <td>{candidate.voteCount}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
